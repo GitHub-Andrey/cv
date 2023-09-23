b3ae61a117ba7f63049db8140509507e5e7644f5
GitHub-Andrey <78257276+GitHub-Andrey@users.noreply.github.com>
2023-09-23 16:48:41 +0200

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,36 @@
+const data = {
+    "first_name": "Andrii",
+    "last_name": "Bilyi",
+    "job_title": "Frontend Developer",
+    "photo": "images/image.jpg",
+    "phone": "+4901605971655",
+    "email": "smotriklol@gmail.com",
+    "skype": "sluchai853",
+    "linkedin": "https://www.linkedin.com/in/andrey-belyi-2360a2263",
+    "address": "26 Siemensstasse, Stuttgart",
+    "skills": [
+        "HTML & HTML5",
+        "CSS & Flex & Grid",
+        "Responsive layout",
+        "Bootstrap",
+        "jQuery",
+        "Gulp",
+        "JavaScript",
+        "Cycles",
+        "Arrays",
+        "DOM",
+        "Object",
+        "Touch",
+        "API",
+        "AJAX",
+        "Deutsch Sprache",
+    ],
+    "education": [
+        ["2022-2023", "ITGID Education Platform, HTML for JS developers"],
+        ["2022-2023", "ITGID Education Platform, JavaScript 2.0"],
+    ],
+    "achievements": [
+        "My projects, that I have completed"
+    ],
+    "profile": "I left my previous job, because I wanted to work in a job, that would be interesting for me. I got interested in programming, studied technology, and all my free time I do various projects, that develop me and that can also show the real level of my knowledge. "
+};
---
+++ js/experience.js
@@ -0,0 +1,29 @@
+const experience = [
+    {
+        "company": "Lending",
+        "city": "Stuttgart",
+        "from": "2023",
+        "to": 'Present',
+        "position": "Description",
+        "description": "This is a one-page website that you can use for your business. It is created exclusively with the help of technologies: HTML, CSS, Flexbox. I used the following services to work with the layout: Figpea, Marsy. And also the program Gimp.",
+        "link": "https://github-andrey.github.io/Lending-Page-Simple/"
+    },
+    {
+        "company": "weather_forecast",
+        "city": "Stuttgart",
+        "from": "2023",
+        "to": 'Present',
+        "position": "Description",
+        "description": "This is an example of a simple weather forecast layout. The main logic is written in JavaScript. Some HTML and CSS languages are used. Using the JavaScript logic from this project, you can add a weather forecast to any website by changing only the layout itself.",
+        "link": "https://github-andrey.github.io/weather_forecast/"
+    },
+    {
+        "company": "My CV",
+        "city": "Stuttgart",
+        "from": "2023",
+        "to": 'Present',
+        "position": "Description",
+        "description": "It's the project that is this summary. It is written using the following technologies: HTML, CSS, Bootstrap. But the use of Bootstrap is key in the responsive layout of this project.  It is an important technology that gives the opportunity to speed up the creation of adaptive websites.",
+        "link": ""
+    },
+]
